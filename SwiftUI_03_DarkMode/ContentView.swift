ad61b2e04243e41bf988a9455a37f43ada5ae12e
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-26 13:10:34 +0200

    [FEATURE][DARK MODE] Main view was modified to get a button to change from dark to light mode

--- SwiftUI_03_DarkMode/ContentView.swift
+++ SwiftUI_03_DarkMode/ContentView.swift
@@ -2,17 +2,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var mode: ColorScheme = .light
+    @State private var change: Bool = true
+
     var body: some View {
         NavigationView {
-            HStack {
-                Image(systemName: "moon.circle")
-                    .font(.title)
-                Text("Dark Mode")
-                    .font(.title)
-                    .bold()
-                    .foregroundColor(Color("textColor"))
+            VStack {
+                HStack {
+                    Image(systemName: "moon.circle")
+                        .font(.title)
+                    Text("Dark Mode")
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(Color("textColor"))
+                }
+                Image("logo")
+                
+                Button(action: {
+                    self.change.toggle()
+                    self.changeMode()
+                }) {
+                    Text("Cambiar modo")
+                }
+                
             }
             .navigationBarTitle("User Defaults")
+
+        }
+        .environment(\.colorScheme, self.mode)
+    }
+}
+
+extension ContentView {
+    func changeMode () {
+        if self.change {
+            self.mode = .light
+        } else {
+            self.mode = .dark
         }
     }
 }
